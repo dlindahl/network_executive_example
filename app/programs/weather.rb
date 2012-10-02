f11d678ea9020aed61df0b8a5e9a0d85561087be
Derek Lindahl <dlindahl@customink.com>
2012-10-02 01:52:33 -0400

    Changed the Weather URL to something a bit fancier

--- app/programs/weather.rb
+++ app/programs/weather.rb
@@ -1,5 +1,5 @@
 class Weather < NetworkExecutive::Program
   def url
-    'http://weather.com'
+    'http://www.wunderground.com/cgi-bin/findweather/getForecast?brand=wxmap&query=38.93844,-77.16538&lat=38.93844&lon=-77.16538&zoom=11&type=terrain&units=english&rad=0&sat=0&svr=0&cams=0&tor=0&wxsn=1&wxsn.mode=tw&wxsn.bcdgtemp=false&wxsn.opa=50&pep=0'
   end
 end
